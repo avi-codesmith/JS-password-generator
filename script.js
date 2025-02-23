610715350abe4dc7132fd2e4b282425e2b97a299
avi-codesmith <avi.codesmith@gmail.com>
2025-02-23 18:29:58 +0530

    Complete the project

--- script.js
+++ script.js
@@ -10,20 +10,35 @@ const lowerCase = "abcdefghijklmnopqrstwxyz";
 const number = "0123456789";
 const symbol = "!@#$%^&(){}[]?/|_+*-";
 
-const ExtraChar = upperCase + lowerCase + number + symbol;
+const extraChars = upperCase + lowerCase + number + symbol;
 
 function createPassword() {
+  smallInput.disabled = false;
   let password = "";
   password += upperCase[Math.floor(Math.random() * upperCase.length)];
   password += lowerCase[Math.floor(Math.random() * lowerCase.length)];
   password += number[Math.floor(Math.random() * number.length)];
   password += symbol[Math.floor(Math.random() * symbol.length)];
 
-  while (length > password.length) {}
+  while (length > password.length) {
+    password += extraChars[Math.floor(Math.random() * extraChars.length)];
+  }
 
-  console.log(password);
+  copyBtn.style.visibility = "visible";
+  smallInput.value = password;
+}
+
+function copyText() {
+  password.select();
+  document.execCommand("copy");
+  alert("Password coppied");
+  location.reload();
 }
 
 button.addEventListener("click", () => {
   createPassword();
+});
+
+copyBtn.addEventListener("click", () => {
+  copyText();
 });
